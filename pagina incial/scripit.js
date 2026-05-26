2d78d1e04774ad693025ff46aa628803cd41c6a0
Sávio Monteiro <saviomonteiro2005@gmail.com>
2026-05-25 20:32:41 -0500

    feat: finalizar tela de recuperar senha

--- pagina incial/scripit.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    const senhaInput = document.getElementById('senha');
-    const togglesenha = document.getElementById('toggleSenha')
-
-    togglesenha.addEventListener('Click', () =>{
-        console.log('O botão foi clicado!')
-    });
-});
